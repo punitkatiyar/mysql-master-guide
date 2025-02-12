7e79e66011b3d626d0f2b036b10596a78458a4d0
Punit Katiyar <punit.kat@gmail.com>
2025-02-12 11:16:07 +0530

    Update delete.js
    Signed-off-by: Punit Katiyar <punit.kat@gmail.com>

--- node/delete.js
+++ node/delete.js
@@ -1,1 +1,8 @@
+const deleteUser = `DELETE FROM users WHERE email = ?`;
+const values = ["john@example.com"];
+
+connection.query(deleteUser, values, (err, results) => {
+    if (err) throw err;
+    console.log("User deleted:", results.affectedRows);
+});
 
