32068f25fbd8d4db43b3cf18d909708d9a0a040f
Punit Katiyar <punit.kat@gmail.com>
2025-02-12 11:15:11 +0530

    Update insert.js
    Signed-off-by: Punit Katiyar <punit.kat@gmail.com>

--- node/insert.js
+++ node/insert.js
@@ -1,1 +1,7 @@
+const insertUser = `INSERT INTO users (name, email) VALUES (?, ?)`;
+const values = ["John Doe", "john@example.com"];
 
+connection.query(insertUser, values, (err, results) => {
+    if (err) throw err;
+    console.log("User inserted, ID:", results.insertId);
+});
