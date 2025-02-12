c578c039db17b25355c1f6577694cdcde3655485
Punit Katiyar <punit.kat@gmail.com>
2025-02-12 11:15:51 +0530

    Update update.js
    Signed-off-by: Punit Katiyar <punit.kat@gmail.com>

--- node/update.js
+++ node/update.js
@@ -1,1 +1,8 @@
+const updateUser = `UPDATE users SET name = ? WHERE email = ?`;
+const values = ["Jane Doe", "john@example.com"];
+
+connection.query(updateUser, values, (err, results) => {
+    if (err) throw err;
+    console.log("User updated:", results.affectedRows);
+});
 
